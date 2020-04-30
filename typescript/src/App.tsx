93283cf1ff08d7ba39007c2cec2f47d9cc48c629
Alekskarb <korbutowski@gmail.com>
2020-04-30 19:37:20 +0300

    'minus idea'

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let a: number | null | string = 10;
+a = "100";
+let name: string = "1e34gtg";
+let isStudent: boolean = true;
+
+let family: Array<string> = ['sanya', 'tanya', 'pasha', 'illya'];
+
+let user = {
+  greeting() {alert('yo')  },
+  name: 'alex',
+  age: 38,
+  isStudent: true
+};
+
 
 export default App;
